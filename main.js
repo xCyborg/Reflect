f0af00b862604b891a36e56b30d6ed2fdda82e7c
xCyborg <caylsharp@gmail.com>
2025-07-04 22:48:46 +0100

    First Web Site (Vue, Quasar)

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  const { createApp } = Vue;
+
+  const appData = () => ({
+    clicked: false, // STATE
+    lorem: null,   
+  });
+
+  const app = createApp({
+    data: appData, 
+    methods: {fetchQuote}
+  });
+
+  app.use(Quasar);
+  app.mount('#app');
+
+
+  async function fetchQuote() {
+    console.log('fetchQuote called');
+    const res = await fetch('https://api.api-ninjas.com/v1/quotes', {
+      headers: {'X-Api-Key': 'uD+lwzckMKXQfGcqRoIvjg==iQTwRGi1F2iFNLII'}
+    });
+    const data = await res.json();
+    this.lorem = data[0];
+  }
+};
